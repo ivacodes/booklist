11d6482fe4a07614abf325292c5b067f37195044
Iva Bozic <ivabozic23@gmail.com>
2021-03-02 17:11:03 +0000

    finished before reducers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import BookContextProvider from "./context/BookContext";
 import { Navbar } from "./components/Navbar";
+import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
 
 function App() {
   return (
     <div className='App'>
       <BookContextProvider>
         <Navbar />
+        <BookList />
+        <BookForm />
       </BookContextProvider>
     </div>
   );
